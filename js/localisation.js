deb0a9b99ae64c750c75948a4c085b7d389a62ac
Victor Gonzalez <victor.gonzalez@protonmail.com>
2022-02-08 12:46:21 +0100

    Tweaks in localisation.js

--- js/localisation.js
+++ js/localisation.js
@@ -11,12 +11,10 @@ class Global {
 
   values = {
     "en": {
-      "genericError": "Something went wrong, please try again",
-      "genericErrorGetInTouch": "Please, refresh page and get in touch with support if the error persists. Error: ",
+      "genericError": "Something went wrong, please try again.",
+      "genericErrorGetInTouch": "An unexpected error has occurred. Please, refresh the page and try it again. Get in touch with support if the error persists. Error: ",
       "feedbackSaving": "Nothing to save...",
 
-      "globalImage": "Image",
-      "globalVideo": "Video",
       "globalRemove": "Remove",
       "globalPreview": "Preview",
       "globalUpdate": "Update",
@@ -57,15 +55,13 @@ class Global {
       "editorVideoUrlTitle": "Video URL? (Only YouTube for now)",
       "editorSectionRemove": "Remove from article",
       "editorSectionMoveUp": "Move Up",
-      "editorSection:MoveDown": "Move Down"
+      "editorSectionMoveDown": "Move Down",
     },
     "es": {
-      "genericError": "Ha ocurrido un error inesperado, por favor inténtalo de nuevo",
-      "genericErrorGetInTouch": "Por favor, actualiza la página y ponte en contacto con el soporte técnico si el error continúa. Error: ",
+      "genericError": "Ha ocurrido un error inesperado, por favor inténtalo de nuevo.",
+      "genericErrorGetInTouch": "Ha ocurrido un error inesperado. Por favor, actualiza la página e intétalo de nuevo. Ponte en contacto con el soporte técnico si el error continúa. Error: ",
       "feedbackSaving": "Nada que guardar...",
 
-      "globalImage": "Imagen",
-      "globalVideo": "Vídeo",
       "globalRemove": "Eliminar",
       "globalPreview": "Previsualizar",
       "globalUpdate": "Actualizar",
@@ -106,15 +102,13 @@ class Global {
       "editorVideoUrlTitle": "Introduce la direccioón URL del vídeo (Solo YouTube por ahora)",
       "editorSectionRemove": "Eliminar sección del artículo",
       "editorSectionMoveUp": "Mover para arriba",
-      "editorSection:MoveDown": "Mover para abajo"
+      "editorSectionMoveDown": "Mover para abajo",
     },
     "gl": {
-      "genericError": "Algo non foi ben, por favor inténtao de novo",
-      "genericErrorGetInTouch": "Por favor, actualiza a páxina e ponte en contacto co soporte técnico se o erro persiste. Erro: ",
+      "genericError": "Algo non foi ben, por favor inténtao de novo.",
+      "genericErrorGetInTouch": "Erro inesperado. Por favor, actualiza a páxina e inténtao de novo. Ponte en contacto co soporte técnico se o erro persiste. Erro: ",
       "feedbackSaving": "Nada que gardar...",
 
-      "globalImage": "Imaxe",
-      "globalVideo": "Vídeo",
       "globalRemove": "Eliminar",
       "globalPreview": "Previsualizar",
       "globalUpdate": "Actualizar",
@@ -155,7 +149,7 @@ class Global {
       "editorVideoUrlTitle": "Introduce o enderezo do vídeo (Só YouTube polo momento)",
       "editorSectionRemove": "Eliminar do artigo",
       "editorSectionMoveUp": "Mover para arriba",
-      "editorSection:MoveDown": "Move para abaixo",
+      "editorSectionMoveDown": "Move para abaixo",
     }
   }
 
